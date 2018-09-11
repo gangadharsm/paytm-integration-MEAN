77e6a8fe1b8fbf3968be13c30b71fda28e33fa8f
Gangadhara <gangadhara@bootminds.com>
2018-09-11 15:29:44 +0530

    paytm done

--- server/server.js
+++ server/server.js
@@ -3,17 +3,46 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const ejs = require("ejs");
 const app = express();
+require("dotenv").config();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+
+const {initPayment, responsePayment} = require("./paytm/services/index");
+
+app.use(cors());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-    res.send('hello from server');
-})
+app.use(express.static(__dirname + "/views"));
+app.set("view engine", "ejs");
+
+app.get("/paywithpaytm", (req, res) => {
+    console.log(req.query.amount)
+    initPayment(req.query.amount).then(
+        success => {
+            console.log(success)
+            res.render("paytmRedirect.ejs", {
+                resultData: success,
+                paytmFinalUrl: process.env.PAYTM_FINAL_URL
+            });
+        },
+        error => {
+            res.send(error);
+        }
+    );
+});
+
+app.post("/paywithpaytmresponse", (req, res) => {
+    responsePayment(req.body).then(
+        success => {
+            res.render("response.ejs", {resultData: "true", responseData: success});
+        },
+        error => {
+            res.send(error);
+        }
+    );
+});
 
 app.listen(PORT, () => {
     console.log("Running on " + PORT);
