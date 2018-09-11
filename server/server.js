6870f4ee6f0871ddd75b5ae832c9597759916de2
Gangadhara <gangadhara@bootminds.com>
2018-09-11 14:45:45 +0530

    cors and ejs added

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const ejs = require("ejs");
 const app = express();
 
 const PORT = 4000;
